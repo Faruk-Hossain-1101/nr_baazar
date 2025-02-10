0d744c894026d65803c7105ba6becc1d238f867c
Faruk Hossain <hossainf114@gmail.com>
2025-02-10 12:37:52 +0530

    Invoice updated to after sell

--- shop/views/sell.py
+++ shop/views/sell.py
@@ -13,6 +13,15 @@ from django.db import transaction
 from django.db.models import F
 
 
+# Function to convert non-serializable types
+def custom_serializer(obj):
+    if isinstance(obj, Decimal):  # Convert Decimal to float
+        return float(obj)
+    if isinstance(obj, datetime.date) or isinstance(obj, datetime.datetime):  # Convert datetime to string
+        return obj.strftime("%Y-%m-%d")
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+
 def generate_invoice_number():
     # Generate a unique invoice number using UUID
     no = f"NRB-{uuid.uuid4().hex.upper()[:8]}"
@@ -40,7 +49,7 @@ def get_product_details(request):
     else:
         discount = 0  # Default to no discount if type is invalid
 
-    return JsonResponse({
+    context = {
         'success': True,
         'product_name': product.name,
         'rate': product.actual_price,
@@ -48,7 +57,8 @@ def get_product_details(request):
         'color': product.color,
         'size': product.size,
         'sku':product.sku
-    })
+    }
+    return JsonResponse(context)
 
 def check_discount(request):
     discount_amount = request.GET.get('discount_amount', '')
@@ -93,10 +103,6 @@ def check_qty(request):
     
 @transaction.atomic
 def show_invoice(request):
-    cart_data = request.session.get("cart", {})
-    customer_data = cart_data.get("customer", {})
-    coupon_data = cart_data.get("coupon", {})
-    
     if request.method == 'POST':
         data = json.loads(request.body)
         cart = data.get('cart', {})
@@ -107,109 +113,125 @@ def show_invoice(request):
         # Return a JSON response to confirm the cart update
         return JsonResponse({'success': True, 'cart': cart})
     
-    # Get the current UTC time and adjust for Kolkata timezone (UTC+5:30)
-    current_time_utc = datetime.utcnow()
-    kolkata_time = current_time_utc + timedelta(hours=5, minutes=30)
+    if request.method == "GET":
+        cart_data = request.session.get("cart", {})
+        coupon_data = cart_data.get("coupon", {})
+        
+        # Get the current UTC time and adjust for Kolkata timezone (UTC+5:30)
+        current_time_utc = datetime.utcnow()
+        kolkata_time = current_time_utc + timedelta(hours=5, minutes=30)
 
 
-    total_item = 0
-    item_discount = Decimal(0)  # Using Decimal for item_discount
+        total_item = 0
+        item_discount = Decimal(0)  # Using Decimal for item_discount
+        actual_price = Decimal(0)
+        # Calculate total discount (discount * quantity for each item)
+        for item in cart_data.get('items', []):
+            actual_price += Decimal(item.get('rate', 0)) * int(item.get('qty', 1))
+            # Calculate the discount for the current item (discount * qty)
+            item_discount += Decimal(item.get('discount', 0)) * int(item.get('qty', 1))
+            total_item += int(item.get('qty', 1))
 
-    # Calculate total discount (discount * quantity for each item)
-    for item in cart_data.get('items', []):
-        # Calculate the discount for the current item (discount * qty)
-        item_discount += Decimal(item.get('discount', 0)) * int(item.get('qty', 1))
-        # Calculate the total quantity of items
-        total_item += int(item.get('qty', 1))
+        # Get total amount after discount
+        total_amount = actual_price - item_discount
+        paid_amount = Decimal(cart_data.get("paidAmount", 0))
+        coupon_discount = Decimal(coupon_data.get('amount', 0))
+        roundoff = Decimal(cart_data.get('roundOff', 0))
 
-    total_discount = item_discount + Decimal(cart_data.get('roundOff', 0))
+        # Calculate grand total after all discounts
+        grand_total = total_amount - (coupon_discount+roundoff)
+        if paid_amount <= 0:
+            due_amount = Decimal(0)
+        else:
+            due_amount = grand_total - paid_amount
 
-    # Get total amount after discount
-    total_amount = Decimal(cart_data.get("totalAmount", 0))
-    paid_amount = Decimal(cart_data.get("paidAmount", 0))
-    coupon_discount = Decimal(coupon_data.get('amount', 0))
-
-    # Calculate grand total after all discounts
-    grand_total = total_amount - (total_discount + coupon_discount)
-    if paid_amount <= 0:
-        due_amount = Decimal(0)
-    else:
-        due_amount = grand_total - paid_amount
-
-     # Quantize all amounts to two decimal places
-    total_discount = total_discount.quantize(Decimal('0.00'))
-    grand_total = grand_total.quantize(Decimal('0.00'))
-    total_amount = total_amount.quantize(Decimal('0.00'))
-    coupon_discount = coupon_discount.quantize(Decimal('0.00'))
-    due_amount = due_amount.quantize(Decimal('0.00'))
-    paid_amount = paid_amount.quantize(Decimal('0.00'))
-
-
-    # Generate unique invoice number and formatted date-time
-    invoice_number = generate_invoice_number()
-    formatted_date = kolkata_time.strftime('%d/%m/%Y')
+        # Quantize all amounts to two decimal places
+        actual_price = actual_price.quantize(Decimal('0.00'))
+        item_discount = item_discount.quantize(Decimal('0.00'))
+        total_amount = total_amount.quantize(Decimal('0.00'))
+        grand_total = grand_total.quantize(Decimal('0.00'))
+        coupon_discount = coupon_discount.quantize(Decimal('0.00'))
+        roundoff = roundoff.quantize(Decimal('0.00'))
+        due_amount = due_amount.quantize(Decimal('0.00'))
+        paid_amount = paid_amount.quantize(Decimal('0.00'))
 
 
-    # Now create the customer, order, and order items
-    customer, created = Customer.objects.get_or_create(
-        phone=customer_data.get('phone', ''),
-        defaults={'name': customer_data.get('name', ''), 'address': customer_data.get('address', '')}
-    )
+        # Generate unique invoice number and formatted date-time
+        invoice_number = generate_invoice_number()
+        formatted_date = kolkata_time.strftime('%d/%m/%Y')
 
-    # Found Coupon
-    coupon = None
-    if coupon_data:
-        coupon = Coupon.objects.filter(code=coupon_data.get('code')).first()
+        context = {
+            "cart_data": cart_data,
+            'actual_price': actual_price,
+            "item_discount": item_discount,
+            "total_amount": total_amount,
+            "grand_total": grand_total,
+            "coupon_discount": coupon_discount,
+            "roundoff": roundoff,
+            "total_item": total_item,
+            "paid_amount": paid_amount,
+            "due_amount": due_amount,
+            "invoice_no": invoice_number,
+            "invoice_date": formatted_date,
+        }  
+        request.session['context'] = json.dumps(context, default=custom_serializer)
+        return render(request, "shop/sell/invoice.html", context)
 
-        if coupon.coupon_type == 'individual':
-            CustomerCoupon.objects.filter(customer=customer).update(is_used = True)
+def after_bill_print(request): 
+    if request.method == "GET":
+        cart_data = request.session.get("cart", {})
+        customer_data = cart_data.get("customer", {})
+        coupon_data = cart_data.get("coupon", {})
+        context = json.loads(request.session.get('context', '{}'))
 
-
-    # Create the order
-    order = Order.objects.create(
-        order_number=invoice_number,
-        customer=customer,
-        total_amount=total_amount+total_discount+coupon_discount,
-        discount_amount=total_discount+coupon_discount,
-        actual_amount=total_amount,
-        paid_amount=paid_amount if paid_amount > 0 else total_amount,
-        due_amount=due_amount,
-        payment_type=cart_data.get('paymentType', 'cash'),  # Adjust if using a different payment type
-        payment_status='Success',  # You can change this if needed
-        coupon=coupon,
-    )
-
-    # Create order items
-    for item in cart_data.get('items', []):
-        qty = int(item.get('qty', 1))
-        Product.objects.filter(sku=item.get('sku', '')).update(stock_quantity=F('stock_quantity') - qty)
-
-        OrderItem.objects.create(
-            order=order,
-            product_name=item.get('productName', ''),
-            actual_price=Decimal(item.get('rate', 0)),
-            discount_amount=Decimal(item.get('discount', 0)),
-            color=item.get('color', ''),
-            size=item.get('size', ''),
-            quantity=qty,
-            sku=item.get('sku', ''),
+        # Now create the customer, order, and order items
+        customer, created = Customer.objects.get_or_create(
+            phone=customer_data.get('phone', ''),
+            defaults={'name': customer_data.get('name', ''), 'address': customer_data.get('address', '')}
         )
 
-    
-    context = {
-        "cart_data": cart_data,
-        "total_discount": total_discount,
-        "grand_total": grand_total,
-        "total_amount": total_amount,
-        "coupon_discount": coupon_discount,
-        "total_item": total_item,
-        "paid_amount": paid_amount,
-        "due_amount": due_amount,
-        "invoice_no": invoice_number,
-        "invoice_date": formatted_date,
-    }
-    return render(request, "shop/sell/invoice.html", context)
+        # Found Coupon
+        coupon = None
+        if coupon_data:
+            coupon = Coupon.objects.filter(code=coupon_data.get('code')).first()
 
-def after_bill_print(request):    
-    request.session['cart'] = None
-    return JsonResponse({"success": True, "message": "Order created successfully"})
+            if coupon.coupon_type == 'individual':
+                CustomerCoupon.objects.filter(customer=customer).update(is_used = True)
+
+
+        # Create the order
+        order = Order.objects.create(
+            order_number=context['invoice_no'],
+            customer=customer,
+            total_amount=context['actual_price'],
+            discount_amount=context['roundoff']+context['coupon_discount']+context['item_discount'],
+            actual_amount=context['grand_total'],
+            paid_amount=context['paid_amount'] if context['paid_amount'] > 0 else context['grand_total'],
+            due_amount=context['due_amount'],
+            payment_type=cart_data.get('paymentType', 'cash'),  # Adjust if using a different payment type
+            payment_status='Success',  # You can change this if needed
+            coupon=coupon,
+        )
+
+        # Create order items
+        for item in cart_data.get('items', []):
+            qty = int(item.get('qty', 1))
+            Product.objects.filter(sku=item.get('sku', '')).update(stock_quantity=F('stock_quantity') - qty)
+
+            OrderItem.objects.create(
+                order=order,
+                product_name=item.get('productName', ''),
+                actual_price=Decimal(item.get('rate', 0)),
+                discount_amount=Decimal(item.get('discount', 0)),
+                color=item.get('color', ''),
+                size=item.get('size', ''),
+                quantity=qty,
+                sku=item.get('sku', ''),
+            )
+
+        
+           
+        request.session['cart'] = None
+        request.session['context'] = None
+        
+        return JsonResponse({"success": True, "message": "Order created successfully"})
